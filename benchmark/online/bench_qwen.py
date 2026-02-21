fb4ab01275222f7600da863e3255afdfdcb465fb
YzXiao101 <XiaoZhuoOps@outlook.com>
2026-02-21 15:41:10 +0800

    feat: temp benchmark

--- benchmark/online/bench_qwen.py
+++ benchmark/online/bench_qwen.py
@@ -49,7 +49,7 @@ async def main(mode: str):
     random.seed(42)  # reproducibility
     PORT = 1919
     N = 1000
-    SCALES = [0.4, 0.5, 0.6, 0.7, 0.8, 1.6]  # from fast to slow
+    SCALES = [0.4, 1.0, 1.6]  # from fast to slow
     async with OpenAI(base_url=f"http://127.0.0.1:{PORT}/v1", api_key="") as client:
         MODEL = await get_model_name(client)
         tokenizer = AutoTokenizer.from_pretrained(MODEL)
